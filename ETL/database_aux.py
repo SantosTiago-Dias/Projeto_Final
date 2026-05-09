77e56d4c1159385926bf1d65c3b7f0f41dd12bcc
Tiago <tiago@example.com>
2026-05-09 09:33:24 +0100

    Fix:correcao na normalizacao

--- ETL/database_aux.py
+++ ETL/database_aux.py
@@ -130,9 +130,9 @@ def execute_transformacao():
         logger.info("A executar: normalizar_lookup")
 
         normalizacoes = [
-            ('DB_CP.detalhes_contratos_transf', 'descricao'),
-            ('DB_CP.detalhes_contratos_transf', 'objeto'),
-            ('DB_CP.entidade_transf', 'nome')
+            ('detalhes_contratos_transf', 'descricao'),
+            ('detalhes_contratos_transf', 'objeto'),
+            ('entidade_transf', 'nome')
             # ('DB_CP.outra_tabela', 'outra_coluna')
         ]
 
