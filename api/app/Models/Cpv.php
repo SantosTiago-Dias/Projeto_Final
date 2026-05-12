24e46693f292bd79b1d8693bfacc6f3010dde1e4
ICS-15 <inezita2003@gmail.com>
2026-05-12 11:39:27 +0100

    Fix : Passar a chamar as dims dos dict no backend

--- api/app/Models/Cpv.php
+++ api/app/Models/Cpv.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Cpv extends Model
 {
     protected $connection = 'etl';
-    protected $table = 'cpv_dictionary';
+    protected $table = 'cpv_dim';
     public $timestamps = false;
 }
