f5790c2ce9137aacc7a681e12709507a13441976
Tiago Santos <tiago.m.santos.201323@gmail.com>
2026-06-30 16:08:59 +0100

    Fix: na retentativa de extração de entidades

--- ETL/Entity_retries.py
+++ ETL/Entity_retries.py
@@ -17,16 +17,16 @@ def main():
         logger.info("Não existem entidades por atualizar.")
         return
 
-    if len(list_entity_not_found) >= 5:
-        for entity_id in list_entity_not_found:
-            log_id = db.change_status(None, TABLE_LOGS,TABLE_NAME, "INICIO","Retry Entity:{entity_id}")
-            logger.info(f"A tentar novamente extrair dados da entidade: {entity_id}")
-            detalhes = extracao_entidades.extrair_detalhes(entity_id)
-            if detalhes:
-                db.insert_data_table(TABLE_NAME, [extracao_entidades.prepare_data(detalhes)])
-                db.change_status(log_id,TABLE_LOGS, None, "SUCESSO", f"Entidade {entity_id} atualizada com sucesso.")
-                logger.info(f"Entidade {entity_id} atualizada com sucesso.")
-            else:
-                db.change_status(log_id,TABLE_LOGS, None, "ERRO", f"Falha ao tentar extrair novamente dados da entidade {entity_id}.")
-                logger.error(f"Falha ao extrair dados da entidade {entity_id}")
+    
+    for entity_id in list_entity_not_found:
+        log_id = db.change_status(None, TABLE_LOGS,TABLE_NAME, "INICIO","Retry Entity:{entity_id}")
+        logger.info(f"A tentar novamente extrair dados da entidade: {entity_id}")
+        detalhes = extracao_entidades.extrair_detalhes(entity_id)
+        if detalhes:
+            db.insert_data_table(TABLE_NAME, [extracao_entidades.prepare_data(detalhes)])
+            db.change_status(log_id,TABLE_LOGS, None, "SUCESSO", f"Entidade {entity_id} atualizada com sucesso.")
+            logger.info(f"Entidade {entity_id} atualizada com sucesso.")
+        else:
+            db.change_status(log_id,TABLE_LOGS, None, "ERRO", f"Falha ao tentar extrair novamente dados da entidade {entity_id}.")
+            logger.error(f"Falha ao extrair dados da entidade {entity_id}")
             
